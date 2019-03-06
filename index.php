4af575de70e687cc7da4f31e83512daffaf42655
Kaloyan Aleksiev <heuckam@gmail.com>
2019-03-06 17:06:24 +0200

    Add index.php file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Fight Arena</title>
+    </head>
+    <body>
+        <p>
+            If you're feeling ready to fight, click the button below!
+        </p>
+        <form method="post">
+            <input type="submit" name="submit" class="submit" value="Fight!" />
+        </form>
+    </body>
+</html>
